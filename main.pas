e4820de402f1f00d576ca16e430b98894b6bead4
Luis Fedrizze <luisfedrizze@gmail.com>
2019-01-22 03:50:16 -0200

    playerclass ultimate metter

--- main.pas
+++ main.pas
@@ -1,4 +1,4 @@
-uses Collider, Payload;
+uses Collider, Payload, PlayerClass;
 
 var 
     Payload:TPayload;
@@ -18,25 +18,40 @@ begin
 
 end;
 
-procedure RenderPlayerUI(Player: TActivePlayer; UltimateLevel:Byte);
+procedure RenderPlayerUI(Player: TActivePlayer);
     var i:Byte;
+        calc:Smallint;
         ultColor:Longint;
+        playerClass:TPlayerClass;
+        playerUltimate:TUltimate;
+        percentage:Byte;
 begin
 
     // Ultimate bar
-    Player.BigText(100, '||', 120, RGB(0,0,0), 0.1, 10, 383);
-    Player.BigText(101, '||', 120, RGB(0,0,0), 0.1, 10, 372);
-    Player.BigText(102, '||', 120, RGB(0,0,0), 0.1, 10, 361);
-    Player.BigText(103, '||', 120, RGB(0,0,0), 0.1, 10, 350);
-    Player.BigText(104, '||', 120, RGB(0,0,0), 0.1, 10, 339);
-    Player.BigText(105, '||', 120, RGB(0,0,0), 0.1, 10, 328);
+    Player.BigText(100, '||', 61, RGB(0,0,0), 0.1, 10, 383);
+    Player.BigText(101, '||', 61, RGB(0,0,0), 0.1, 10, 372);
+    Player.BigText(102, '||', 61, RGB(0,0,0), 0.1, 10, 361);
+    Player.BigText(103, '||', 61, RGB(0,0,0), 0.1, 10, 350);
+    Player.BigText(104, '||', 61, RGB(0,0,0), 0.1, 10, 339);
+    Player.BigText(105, '||', 61, RGB(0,0,0), 0.1, 10, 328);
 
-    Player.BigText(106, '_', 120, RGB(0,0,0), 0.08, 12, 318);
-    Player.BigText(107, '_', 120, RGB(0,0,0), 0.08, 12, 384);
+    Player.BigText(106, '_', 61, RGB(0,0,0), 0.08, 12, 318);
+    Player.BigText(107, '_', 61, RGB(0,0,0), 0.08, 12, 384);
+
+    // Desenha os %
+    GetPlayerClass(Player.ID, playerClass);
+    GetUltimate(Player.ID, playerUltimate);
+    if playerClass._created then percentage := playerUltimate.percentage
+    else percentage := 0;
 
     // Barrinhas
     for i:=10 to 22 do begin
-        if UltimateLevel >= i-9 then ultColor := RGB(0,255,0)
+        calc := i-9;
+        calc := round((calc*100)/13);
+        if playerUltimate.isActive then ultColor := RGB(255, 255, 255)
+        else if percentage >= calc then begin 
+            ultColor := RGB(0,255,0);
+        end
         else ultColor := RGB(255,0,0);
         Player.BigText(i, '.', 120, ultColor, 0.24, 10, 298 + (5 * (22-i)));
     end;
@@ -46,50 +61,66 @@ end;
 procedure Update(Ticks: Integer);
 var 
     collisionDetector,i: Byte;
+    playerClass:TPlayerClass;
+    playerUltimate:TUltimate;
 begin
 
     // Init vars
     Payload.isMoving := false;
-    if (Ticks mod 60) = 0 then begin
-        UltLevel := UltLevel + 1;
-    end;
-    if UltLevel > 13 then UltLevel := 0;
-
-    // Render payload
-    RenderPayload();
+    Payload.isContested := false;
     
     // Players events
     for i := 1 to 32 do begin
         
-        // Inside Collider check
         if Players.Player[i].Alive then begin
+            // Inside Collider check
             collisionDetector := CollisionBox_CollideWithXY(Payload.Collider, Players.Player[i].X, Players.Player[i].Y, 10, 10);
             if collisionDetector <> CollisionBox_NONE then Payload.OnPlayerCollision(Players.Player[i], collisionDetector);
-        end;
 
-        // External Collider check
-        if Players.Player[i].Alive then begin
+            // External Collider check
             collisionDetector := CollisionBox_CollideWithXY(Payload.ExternalCollider, Players.Player[i].X, Players.Player[i].Y, 10, 10);
             if collisionDetector = CollisionBox_FULL then begin
                 Payload.OnPlayerExternalCollision(Players.Player[i], collisionDetector);
             end;
+
+            // Update player class
+            GetPlayerClass(Players.Player[i].ID, playerClass);
+            if playerClass._created then begin
+
+                // Get the player ultimate
+                GetUltimate(Players.Player[i].ID, playerUltimate);
+
+                // Update ultimate percentage
+                if not UltimateInstances[Players.Player[i].ID].isActive then begin
+                    if (Ticks mod 60) = 0 then begin 
+                        UltimateInstances[Players.Player[i].ID].percentage := UltimateInstances[Players.Player[i].ID].percentage + 20;
+                    end;
+                    
+                    if UltimateInstances[Players.Player[i].ID].percentage >= 100 then begin 
+                        UltimateInstances[Players.Player[i].ID].percentage := 100;
+                    end;
+                end;
+
+                // Players that activate the ultimate
+                if not (playerUltimate.isActive) and (Players.Player[i].KeyFlagThrow) and (UltimateInstances[Players.Player[i].ID].percentage=100) then UltimateInstances[Players.Player[i].ID].doTheUltimate(Players.Player[i]);
+            end;
+
+            // Update and render player UI
+            RenderPlayerUI(Players.Player[i]);
         end;
-
-        // Players that activate the ultimate
-        // if Players.Player[i].Alive and Players.Player[i].KeyFlagThrow then Players.Player[i].Say('I GOT YOU IN MY SIGHTS!');
-
-        // Update and render player UI
-        if Players.Player[i].Alive then RenderPlayerUI(Players.Player[i], UltLevel);
     end;
 
     // Update payload position
-    if Payload.isMoving = true then begin
+    if Payload.isMoving and not Payload.isContested then begin
         Payload.xVel := Payload.xVel + Payload.velStep;
         if Payload.xVel > Payload.velMax then Payload.xVel := Payload.velMax;
 
         Payload.Collider.X := Payload.Collider.X + Payload.xVel;
         Payload.ExternalCollider.X := Payload.ExternalCollider.X + Payload.xVel;
     end else Payload.xVel := 0;
+
+    // Render payload
+    RenderPayload();
 end;
 
 procedure OnPlayerCollidesOnPayload(Player: TActivePlayer; Side: Byte);
@@ -108,11 +139,16 @@ end;
 procedure OnPlayerCollidesExternalPayloadCollider(Player: TActivePlayer; Side: Byte);
 begin
     if Player.Team = 2 then Payload.isMoving := true;
+    if Player.Team = 1 then Payload.isContested := true;
 end;
 
 procedure OnPlayerCommand (Player: TActivePlayer; Text: string);
 begin
     if Text='!coords' then Player.Tell(floattostr(Player.X) + ',' + floattostr(Player.Y));
+    if Text='!class pyro' then begin
+        Player.Tell('Changing your class to PYRO');
+        CreatePlayerClass(CLASS_TYPE_PYRO, Player);
+    end;
 end;
 
 begin
@@ -132,7 +168,7 @@ begin
     Game.TickThreshold := 1;
     Game.OnClockTick := @Update;
 
-    // Custom 
-    Players.Player[1].OnSpeak := @OnPlayerCommand;
+    // Custom
+    for i:=1 to 32 do Players.Player[i].OnSpeak := @OnPlayerCommand;
 
 end.
